ebc920886b2460bb3e35bf02fa41421e40b34103
Bochin Anton <antonbocin342@gmail.com>
2024-08-13 16:54:28 +0300

    feat: did task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  copyStr = str
+  for(let i = 0; i <= copyStr.length + 2; i++){
+    for (let items of bracketsConfig){
+      while(copyStr.includes(items[0] + items[1])){
+        copyStr = copyStr.replace(items[0] + items[1], '');
+      }
+    }
+  }
+  return !copyStr.length
 }
